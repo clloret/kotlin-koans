628a831f65e9b3b8b6ae9c5e64c0e9f0a46891f1
Carlos Lloret <clloretplayan@gmail.com>
2020-04-26 21:25:58 +0200

    Complete Partition

--- Collections/Partition/src/Task.kt
+++ Collections/Partition/src/Task.kt
@@ -1,2 +1,14 @@
 // Return customers who have more undelivered orders than delivered
-fun Shop.getCustomersWithMoreUndeliveredOrders(): Set<Customer> = TODO()
+fun Shop.getCustomersWithMoreUndeliveredOrders(): Set<Customer> {
+  val set = mutableSetOf<Customer>()
+
+  customers.forEach { customer ->
+    val pair = customer.orders.partition { it.isDelivered }
+
+    if (pair.first.size < pair.second.size) {
+      set.add(customer)
+    }
+  }
+
+  return set
+}
