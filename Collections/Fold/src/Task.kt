8e53acbe36627af6af2dda94df3c99625f583fc7
Carlos Lloret <clloretplayan@gmail.com>
2020-04-28 21:48:02 +0200

    Complete Fold

--- Collections/Fold/src/Task.kt
+++ Collections/Fold/src/Task.kt
@@ -1,7 +1,8 @@
+
 // Return the set of products that were ordered by all customers
 fun Shop.getProductsOrderedByAll(): Set<Product> {
-    TODO()
+    return customers.fold(setOf(customers.first().getOrderedProducts().first())) { acc, customer -> acc.plus(acc.intersect(customer.getOrderedProducts())) }
 }
 
 fun Customer.getOrderedProducts(): List<Product> =
-        TODO()
+        orders.flatMap { it.products }
